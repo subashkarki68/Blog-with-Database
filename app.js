21dd651a005093c5cadecef73c818fdeb2769f46
subash karki <subashkarki68@gmail.com>
2022-07-07 11:43:06 +0545

    complete challange 5

--- app.js
+++ app.js
@@ -23,6 +23,19 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/about', function (req, res) {
+  res.render('about', {
+    aboutContent: aboutContent
+  });
+});
+
+app.get('/contact', function (req, res) {
+  res.render('contact', {
+    contactContent: contactContent
+  });
+});
+
+
 
 app.listen(PORT, function () {
   console.log("Server started on port " + PORT);

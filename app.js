21526512aaa66f5f649314325e26a4c66a9e85c6
subash karki <subashkarki68@gmail.com>
2022-07-07 12:26:17 +0545

    complete challange 10

--- app.js
+++ app.js
@@ -35,7 +35,17 @@ app.get('/contact', function (req, res) {
   });
 });
 
+app.get('/compose', function (req, res) {
+  res.render('compose', {});
+});
 
+app.post('/compose', function (req, res) {
+  let newPost = {
+    postTitle: req.body.postTitle,
+    postBody: req.body.postBody
+  };
+  res.send(newPost);
+});
 
 app.listen(PORT, function () {
   console.log("Server started on port " + PORT);

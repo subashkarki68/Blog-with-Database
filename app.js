720bbb8c8823a881d6166675e2a9d6e9588ed88d
subash karki <subashkarki68@gmail.com>
2022-07-07 11:26:09 +0545

    complete challange 2

--- app.js
+++ app.js
@@ -11,15 +11,19 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
-app.get('/', function(req, res){
-  res.render('home');
+app.get('/', function (req, res) {
+  res.render('home', {
+    homeContent: homeStartingContent
+  });
 });
 
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("Server started on port " + PORT);
 });
